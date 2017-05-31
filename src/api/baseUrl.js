dfc005dd525ea8b8fedadf3ae10d64a7f81eced3
Diego Meneses <dmeneses@ilimitada.co>
2017-05-31 16:31:24 -0500

    ambiente de producción

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,16 @@
 export default function getBaseUrl() {
+  /* v1
   const inDevelopment = window.location.hostname === 'localhost';
   return inDevelopment ? 'http://localhost:3001/' : '/';
+  */
+
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+}
+
+
+
+function getQueryStringParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=((^")
 }
