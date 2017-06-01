1a8b5748abc86e4a1f6ea9b70de31c9167db8f54
diego alberto meneses <diego alberto meneses>
2017-05-31 19:07:03 -0500

    ambiente de producción con npm

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,9 +19,10 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.get('users', function(req, res){
+app.get('/users', function(req, res){
   res.json([
-    {"id": 1,"firstName": "Bob","lastName": "Smith","email":"bob@gmail.com"}
+    {"id": 1,"firstName": "Bob","lastName": "Smith","email":"bob@gmail.com"},
+    {"id": 2,"firstName": "Bob","lastName": "Smith","email":"bob@gmail.com"}
   ]);
 });
 
--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -12,5 +12,9 @@ export default function getBaseUrl() {
 function getQueryStringParameterByName(name, url) {
   if (!url) url = window.location.href;
   name = name.replace(/[\[\]]/g, "\\$&");
-  var regex = new RegExp("[?&]" + name + "(=((^")
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g," "));
 }
